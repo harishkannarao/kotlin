6795e1cf5d5000f25c60f3aa4b3be39f74b3bb9f
harishkannarao <harish.gtec@gmail.com>
2019-03-06 10:23:03 +0000

    Customizing Http Engine to follow redirects

--- ktor/src/main/kotlin/com/harishkannarao/ktor/dependency/Dependencies.kt
+++ ktor/src/main/kotlin/com/harishkannarao/ktor/dependency/Dependencies.kt
@@ -9,12 +9,27 @@ import com.harishkannarao.ktor.intercept.Interceptor
 import com.harishkannarao.ktor.web.user.UserWeb
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
+import io.ktor.client.features.HttpPlainText
 
 class Dependencies(
         config: KtorApplicationConfig,
         overriddenDependencies: OverriddenDependencies = OverriddenDependencies()
 ) {
-    val client = HttpClient(Apache)
+    val client = HttpClient(Apache) {
+        install(HttpPlainText) {
+            defaultCharset = Charsets.UTF_8
+        }
+
+        engine {
+            // For timeouts: 0 means infinite, while negative value mean to use the system's default value
+            socketTimeout = 10_000  // Max time between TCP packets - default 10 seconds
+            connectTimeout = 10_000 // Max time to establish an HTTP connection - default 10 seconds
+            connectionRequestTimeout = 20_000 // Max time for the connection manager to start a request - 20 seconds
+        }
+
+        expectSuccess = false
+        followRedirects = true
+    }
     val interceptor = Interceptor()
     private val clientJsonUtil = ClientJsonUtil()
     private val customerClient = CustomerClient(clientJsonUtil, client, config.thirdPartyCustomerServiceUrl)
