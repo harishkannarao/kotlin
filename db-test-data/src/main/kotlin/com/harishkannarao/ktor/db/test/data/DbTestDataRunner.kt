c06db5295af26863b0580e3dbd1d1b680481e392
harishkannarao <harish.gtec@gmail.com>
2019-05-09 16:28:14 +0100

    Setting up test data using test data runner

--- db-test-data/src/main/kotlin/com/harishkannarao/ktor/db/test/data/DbTestDataRunner.kt
+++ db-test-data/src/main/kotlin/com/harishkannarao/ktor/db/test/data/DbTestDataRunner.kt
@@ -1,11 +1,15 @@
 package com.harishkannarao.ktor.db.test.data
 
 import com.harishkannarao.ktor.config.KtorApplicationConfig
+import com.harishkannarao.ktor.dao.JsonEntityDao
 import com.harishkannarao.ktor.dao.RelationalEntityDao
+import com.harishkannarao.ktor.dao.entity.JsonDbEntity
 import com.harishkannarao.ktor.dao.entity.RelationalEntity
+import com.harishkannarao.ktor.dao.json.DbJsonUtil
 import com.harishkannarao.ktor.jdbi.JdbiFactory
 import com.harishkannarao.ktor.util.DateTimeUtil
 import java.math.BigDecimal
+import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.*
 
@@ -17,8 +21,11 @@ object DbTestDataRunner {
         dataSource.use { ds ->
             val jdbi = JdbiFactory.createJdbi(ds)
             val relationalEntityDao = RelationalEntityDao(jdbi)
+            val dbJsonUtil = DbJsonUtil()
+            val jsonEntityDao = JsonEntityDao(jdbi, dbJsonUtil)
 
             val referenceDateTime = DateTimeUtil.toUtcZoneOffset(OffsetDateTime.now())
+            val referenceDate = LocalDate.now().toEpochDay()
             repeat(10000) {
                 val entity = RelationalEntity(
                         UUID.randomUUID(),
@@ -34,6 +41,21 @@ object DbTestDataRunner {
 
                 )
                 relationalEntityDao.createEntity(entity)
+
+                val jsonDbEntity = JsonDbEntity(
+                        id = UUID.randomUUID(),
+                        username = it.toString(),
+                        dateInEpochDays = referenceDate.minus(it),
+                        timeStampInEpochMillis = referenceDateTime.minusDays(it.toLong()).toInstant().toEpochMilli(),
+                        intField = it,
+                        booleanField = true,
+                        decimalField = it.toBigDecimal().add("0.001".toBigDecimal()),
+                        tags = listOf(it.toString()),
+                        nestedData = listOf(
+                                JsonDbEntity.NestedData(it.toString(), listOf(it.toString()))
+                        )
+                )
+                jsonEntityDao.createEntity(jsonDbEntity)
             }
         }
         println("Test Data setup completed")
