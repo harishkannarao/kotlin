4eeb0d4109c5c898add8dfa37fb343cb96adc0b4
harishkannarao <harish.gtec@gmail.com>
2019-05-14 23:29:41 +0100

    Fine tuning xargs in the script

--- minify-js-css.sh
+++ minify-js-css.sh
@@ -7,9 +7,9 @@ set -e
 set -x
 
 # Delete previously downloaded files if present
-find . -name 'compiler-*.tar.gz' -type f | xargs rm
-find . -name 'closure-compiler.jar' -type f | xargs rm
-find . -name 'closure-compiler-*' -type d | xargs rm -rf
+find . -name 'compiler-*.tar.gz' -type f | xargs -n1 sh -c 'rm $1' sh
+find . -name 'closure-compiler.jar' -type f | xargs -n1 sh -c 'rm $1' sh
+find . -name 'closure-compiler-*' -type d | xargs -n1 sh -c 'rm -rf $1' sh
 
 # Download closure compiler
 CLOSURE_COMPILER_VERSION=20190415
@@ -24,6 +24,6 @@ cp build/closure-compiler-$CLOSURE_COMPILER_VERSION/closure-compiler-v$CLOSURE_C
 
 # minify all javascript files
 
-find ktor/src -name '*.min.js' | xargs rm
+find ktor/src -name '*.min.js' | xargs -n1 sh -c 'rm $1' sh
 
 find ktor/src -name '*.js' ! -name '*.min.js' -type f | sed -nE 's/.js$//p' | xargs -n1 sh -c 'java -version && java -jar build/closure-compiler.jar --js_output_file=$1.min.js $1.js' sh
