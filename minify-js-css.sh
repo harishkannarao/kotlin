4b6a20c73a9d635d7f7c66cca78cca43a8fd00c5
harishkannarao <harish.gtec@gmail.com>
2019-05-15 09:02:57 +0100

    Trying to fix the Travis CI build failure

--- minify-js-css.sh
+++ minify-js-css.sh
@@ -6,15 +6,20 @@ set -e
 # Print the commands as it is executed. Useful for debugging
 set -x
 
+mkdir -p build
+
 # Delete previously downloaded files if present
-find . -name 'compiler-*.tar.gz' -type f | xargs -n1 sh -c 'rm $1' sh
-find . -name 'closure-compiler.jar' -type f | xargs -n1 sh -c 'rm $1' sh
-find . -name 'closure-compiler-*' -type d | xargs -n1 sh -c 'rm -rf $1' sh
+# find build -name 'compiler-*.tar.gz' -type f -print0 | xargs -0 -I {} rm {}
+find build -name 'closure-compiler.jar' -type f -print0 | xargs -0 -I {} rm {}
+find build -name 'closure-compiler-*' -type d -print0 | xargs -0 -I {} rm -rf {}
 
 # Download closure compiler
 CLOSURE_COMPILER_VERSION=20190415
 
-wget "https://dl.google.com/closure-compiler/compiler-$CLOSURE_COMPILER_VERSION.tar.gz" -P build
+if [[ ! -f build/compiler-$CLOSURE_COMPILER_VERSION.tar.gz ]]
+then
+    wget "https://dl.google.com/closure-compiler/compiler-$CLOSURE_COMPILER_VERSION.tar.gz" -P build
+fi
 
 mkdir -p build/closure-compiler-$CLOSURE_COMPILER_VERSION
 
@@ -24,6 +29,6 @@ cp build/closure-compiler-$CLOSURE_COMPILER_VERSION/closure-compiler-v$CLOSURE_C
 
 # minify all javascript files
 
-find ktor/src -name '*.min.js' | xargs -n1 sh -c 'rm $1' sh
+find ktor/src -name '*.min.js' -type f -print0 | xargs -0 -I {} rm {}
 
 find ktor/src -name '*.js' ! -name '*.min.js' -type f | sed -nE 's/.js$//p' | xargs -n1 sh -c 'java -jar build/closure-compiler.jar --js_output_file=$1.min.js $1.js' sh
