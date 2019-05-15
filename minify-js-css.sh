3df333c2a989875e7ff8a88e65a5696dd5cdf641
harishkannarao <harish.gtec@gmail.com>
2019-05-15 09:06:22 +0100

    Changing minifier file to minifier_cache directory and adding the directory to .gitignore

--- minify-js-css.sh
+++ minify-js-css.sh
@@ -6,29 +6,29 @@ set -e
 # Print the commands as it is executed. Useful for debugging
 set -x
 
-mkdir -p build
+mkdir -p minifier_cache
 
 # Delete previously downloaded files if present
-# find build -name 'compiler-*.tar.gz' -type f -print0 | xargs -0 -I {} rm {}
-find build -name 'closure-compiler.jar' -type f -print0 | xargs -0 -I {} rm {}
-find build -name 'closure-compiler-*' -type d -print0 | xargs -0 -I {} rm -rf {}
+# find minifier_cache -name 'compiler-*.tar.gz' -type f -print0 | xargs -0 -I {} rm {}
+find minifier_cache -name 'closure-compiler.jar' -type f -print0 | xargs -0 -I {} rm {}
+find minifier_cache -name 'closure-compiler-*' -type d -print0 | xargs -0 -I {} rm -rf {}
 
 # Download closure compiler
 CLOSURE_COMPILER_VERSION=20190415
 
-if [[ ! -f build/compiler-$CLOSURE_COMPILER_VERSION.tar.gz ]]
+if [[ ! -f minifier_cache/compiler-$CLOSURE_COMPILER_VERSION.tar.gz ]]
 then
-    wget "https://dl.google.com/closure-compiler/compiler-$CLOSURE_COMPILER_VERSION.tar.gz" -P build
+    wget "https://dl.google.com/closure-compiler/compiler-$CLOSURE_COMPILER_VERSION.tar.gz" -P minifier_cache
 fi
 
-mkdir -p build/closure-compiler-$CLOSURE_COMPILER_VERSION
+mkdir -p minifier_cache/closure-compiler-$CLOSURE_COMPILER_VERSION
 
-tar -xvzf build/compiler-$CLOSURE_COMPILER_VERSION.tar.gz -C build/closure-compiler-$CLOSURE_COMPILER_VERSION
+tar -xvzf minifier_cache/compiler-$CLOSURE_COMPILER_VERSION.tar.gz -C minifier_cache/closure-compiler-$CLOSURE_COMPILER_VERSION
 
-cp build/closure-compiler-$CLOSURE_COMPILER_VERSION/closure-compiler-v$CLOSURE_COMPILER_VERSION.jar build/closure-compiler.jar
+cp minifier_cache/closure-compiler-$CLOSURE_COMPILER_VERSION/closure-compiler-v$CLOSURE_COMPILER_VERSION.jar minifier_cache/closure-compiler.jar
 
 # minify all javascript files
 
 find ktor/src -name '*.min.js' -type f -print0 | xargs -0 -I {} rm {}
 
-find ktor/src -name '*.js' ! -name '*.min.js' -type f | sed -nE 's/.js$//p' | xargs -n1 sh -c 'java -jar build/closure-compiler.jar --js_output_file=$1.min.js $1.js' sh
+find ktor/src -name '*.js' ! -name '*.min.js' -type f | sed -nE 's/.js$//p' | xargs -n1 sh -c 'java -jar minifier_cache/closure-compiler.jar --js_output_file=$1.min.js $1.js' sh
