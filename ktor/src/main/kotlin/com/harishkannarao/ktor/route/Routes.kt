2b2bac80a4f41e0b52d00adb68d8bbbf10d914c4
harishkannarao <harish.gtec@gmail.com>
2020-10-10 10:42:53 +0100

    Simplifying routes configurations

--- ktor/src/main/kotlin/com/harishkannarao/ktor/route/Routes.kt
+++ ktor/src/main/kotlin/com/harishkannarao/ktor/route/Routes.kt
@@ -35,14 +35,14 @@ class Routes(
     private val logger = LoggerFactory.getLogger(Routes::class.java)
 
     fun configure(routing: Routing) {
-        routing.route("") {
+        routing.route("/") {
 
             get {
                 logger.info("sample log message to test MDC param")
                 call.respond(TextContent("My Example Blog", ContentType.Text.Plain))
             }
 
-            route("basic-auth-get") {
+            route("/basic-auth-get") {
                 authenticate(Features.BASIC_AUTH) {
                     get {
                         call.respond(TextContent("Successfully authenticated with basic auth", ContentType.Text.Plain))
@@ -51,7 +51,7 @@ class Routes(
             }
 
             if (config.enableSnippetsApi) {
-                route("snippets") {
+                route("/snippets") {
                     get {
                         call.respond(dependencies.snippetsApi.getDefaultSnippets())
                     }
@@ -61,132 +61,132 @@ class Routes(
                     }
                 }
             }
+        }
 
-            route("customer-by-id") {
-                get {
-                    val id = call.request.queryParameters["id"] ?: ""
-                    call.respond(dependencies.customerApi.getCustomerById(id))
-                }
+        routing.route("/customer-by-id") {
+            get {
+                val id = call.request.queryParameters["id"] ?: ""
+                call.respond(dependencies.customerApi.getCustomerById(id))
             }
+        }
 
-            route("customers-by-ids") {
-                get {
-                    val ids = call.request.queryParameters["ids"] ?: ""
-                    call.respond(dependencies.customerApi.getCustomerByIds(ids.split(",")))
-                }
+        routing.route("/customers-by-ids") {
+            get {
+                val ids = call.request.queryParameters["ids"] ?: ""
+                call.respond(dependencies.customerApi.getCustomerByIds(ids.split(",")))
             }
+        }
 
-            route("customers-by-name") {
-                get {
-                    val name = call.request.queryParameters["name"] ?: ""
-                    call.respond(dependencies.customerApi.getCustomersByName(name))
-                }
+        routing.route("/customers-by-name") {
+            get {
+                val name = call.request.queryParameters["name"] ?: ""
+                call.respond(dependencies.customerApi.getCustomersByName(name))
             }
+        }
 
-            route("create-customer") {
-                post {
-                    val input = call.receive<CustomerDto>()
-                    dependencies.customerApi.createCustomer(input)
-                    call.respond(HttpStatusCode.NoContent, Unit)
-                }
+        routing.route("/create-customer") {
+            post {
+                val input = call.receive<CustomerDto>()
+                dependencies.customerApi.createCustomer(input)
+                call.respond(HttpStatusCode.NoContent, Unit)
             }
+        }
 
-            route("create-customers") {
-                post {
-                    val input = call.receive<List<CustomerDto>>()
-                    dependencies.customerApi.createCustomers(input)
-                    call.respond(HttpStatusCode.NoContent, Unit)
-                }
+        routing.route("/create-customers") {
+            post {
+                val input = call.receive<List<CustomerDto>>()
+                dependencies.customerApi.createCustomers(input)
+                call.respond(HttpStatusCode.NoContent, Unit)
             }
+        }
 
-            route("cookie-session") {
-                get {
-                    val session: CookieSession = call.sessions.getOrSet(
-                            generator = { CookieSession(0) }
-                    )
-                    val updatedSession = session.copy(value = session.value + 1)
+        routing.route("cookie-session") {
+            get {
+                val session: CookieSession = call.sessions.getOrSet(
+                        generator = { CookieSession(0) }
+                )
+                val updatedSession = session.copy(value = session.value + 1)
+                call.sessions.set(updatedSession)
+                val sessionCookie: String? = call.request.cookies["COOKIE_NAME"]
+                call.respond(TextContent("SessionId: $sessionCookie, counter: ${updatedSession.value}", ContentType.Text.Plain))
+            }
+        }
+
+        routing.route("header-session") {
+            get {
+                val existingSession = call.sessions.get<HeaderSession>()
+                val session: HeaderSession = when {
+                    existingSession != null -> existingSession
+                    else -> {
+                        call.sessions.clear<HeaderSession>()
+                        HeaderSession(0)
+                    }
+                }
+                val updatedSession = session.copy(value = session.value + 1)
+                if (updatedSession.value > 9) {
+                    call.sessions.clear<HeaderSession>()
+                    call.respond(TextContent("Session invalidated", ContentType.Text.Plain))
+                } else {
+                    call.sessions.clear<HeaderSession>()
                     call.sessions.set(updatedSession)
-                    val sessionCookie: String? = call.request.cookies["COOKIE_NAME"]
-                    call.respond(TextContent("SessionId: $sessionCookie, counter: ${updatedSession.value}", ContentType.Text.Plain))
+                    val sessionHeader: String? = call.request.headers["HEADER_NAME"]
+                    call.respond(TextContent("SessionId: $sessionHeader, counter: ${updatedSession.value}", ContentType.Text.Plain))
                 }
             }
+        }
 
-            route("header-session") {
-                get {
-                    val existingSession = call.sessions.get<HeaderSession>()
-                    val session: HeaderSession = when {
-                        existingSession != null -> existingSession
-                        else -> {
-                            call.sessions.clear<HeaderSession>()
-                            HeaderSession(0)
-                        }
-                    }
-                    val updatedSession = session.copy(value = session.value + 1)
-                    if (updatedSession.value > 9) {
-                        call.sessions.clear<HeaderSession>()
-                        call.respond(TextContent("Session invalidated", ContentType.Text.Plain))
-                    } else {
-                        call.sessions.clear<HeaderSession>()
-                        call.sessions.set(updatedSession)
-                        val sessionHeader: String? = call.request.headers["HEADER_NAME"]
-                        call.respond(TextContent("SessionId: $sessionHeader, counter: ${updatedSession.value}", ContentType.Text.Plain))
-                    }
-                }
-            }
-
-            route("file-echo") {
-                post {
-                    var title = ""
-                    var fileLines: List<String> = emptyList()
-                    if (call.request.contentType().match(ContentType.MultiPart.Any)) {
-                        val multipart = call.receiveMultipart()
-                        multipart.forEachPart { part ->
-                            when (part) {
-                                is PartData.FormItem -> {
-                                    if (part.name == "title") {
-                                        title = part.value
-                                    }
+        routing.route("file-echo") {
+            post {
+                var title = ""
+                var fileLines: List<String> = emptyList()
+                if (call.request.contentType().match(ContentType.MultiPart.Any)) {
+                    val multipart = call.receiveMultipart()
+                    multipart.forEachPart { part ->
+                        when (part) {
+                            is PartData.FormItem -> {
+                                if (part.name == "title") {
+                                    title = part.value
                                 }
-                                is PartData.FileItem -> {
-                                    if (part.name == "text_file") {
-                                        val charset = part.contentType?.charset() ?: StandardCharsets.UTF_8
-                                        part.streamProvider().use { inputStream ->
-                                            fileLines = IOUtils.readLines(inputStream, charset)
-                                        }
+                            }
+                            is PartData.FileItem -> {
+                                if (part.name == "text_file") {
+                                    val charset = part.contentType?.charset() ?: StandardCharsets.UTF_8
+                                    part.streamProvider().use { inputStream ->
+                                        fileLines = IOUtils.readLines(inputStream, charset)
                                     }
                                 }
                             }
-                            part.dispose()
                         }
+                        part.dispose()
                     }
-                    call.respond(TextContent("title: $title, lines: $fileLines", ContentType.Text.Plain))
                 }
+                call.respond(TextContent("title: $title, lines: $fileLines", ContentType.Text.Plain))
             }
+        }
 
-            route("intercept-cookie") {
-                intercept(ApplicationCallPipeline.Features, dependencies.interceptor.knownCookieInterceptor(Interceptor.KNOWN_COOKIE, Interceptor.KNOWN_COOKIE_ATTRIBUTE_KEY))
-                get {
-                    val knownCookie = call.attributes[Interceptor.KNOWN_COOKIE_ATTRIBUTE_KEY]
-                    call.respond(TextContent("Known Cookie: $knownCookie", ContentType.Text.Plain))
-                }
+        routing.route("intercept-cookie") {
+            intercept(ApplicationCallPipeline.Features, dependencies.interceptor.knownCookieInterceptor(Interceptor.KNOWN_COOKIE, Interceptor.KNOWN_COOKIE_ATTRIBUTE_KEY))
+            get {
+                val knownCookie = call.attributes[Interceptor.KNOWN_COOKIE_ATTRIBUTE_KEY]
+                call.respond(TextContent("Known Cookie: $knownCookie", ContentType.Text.Plain))
             }
+        }
 
-            route("user") {
-                get {
-                    call.respond(dependencies.userWeb.displayUser())
-                }
+        routing.route("user") {
+            get {
+                call.respond(dependencies.userWeb.displayUser())
             }
+        }
 
-            route("generate-401") {
-                get {
-                    call.respond(HttpStatusCode.Unauthorized, Unit)
-                }
+        routing.route("generate-401") {
+            get {
+                call.respond(HttpStatusCode.Unauthorized, Unit)
             }
+        }
 
-            route("ping-external") {
-                get {
-                    call.respond(HttpStatusCode.OK, dependencies.thirdPartyApiClient.pingUrl())
-                }
+        routing.route("ping-external") {
+            get {
+                call.respond(HttpStatusCode.OK, dependencies.thirdPartyApiClient.pingUrl())
             }
         }
     }
